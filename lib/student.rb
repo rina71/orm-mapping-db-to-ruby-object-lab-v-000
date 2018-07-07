3b3906d627f321dbe5f6ec74b8a5904672afa018
Yurina Shin <yurinas@gmail.com>
2018-07-07 16:30:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,4 +86,14 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+    SQL
+
+    DB[:conn].execute(sql).first  
+  end
 end
